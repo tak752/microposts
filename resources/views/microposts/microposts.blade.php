98dfc373c94d80673f1c882028f4e40b7b5cc0da
takashi.hiratani <tak752e@gmail.com>
2021-11-06 14:26:12 +0000

    favorite edit1106

--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -14,15 +14,17 @@
                         {{-- 投稿内容 --}}
                         <p class="mb-0">{!! nl2br(e($micropost->content)) !!}</p>
                     </div>
-                    <div>
+                    <div class="d-inline-block">
+                        {{-- お気に入り追加・削除ボタン--}}
+                        @include('microposts_favorite.favorite_button')
+                    </div>
+                    <div class="d-inline-block">
                         @if (Auth::id() == $micropost->user_id)
                             {{-- 投稿削除ボタンのフォーム --}}
                             {!! Form::open(['route' => ['microposts.destroy', $micropost->id], 'method' => 'delete']) !!}
                                 {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-sm']) !!}
                             {!! Form::close() !!}
                         @endif
-                            {{-- お気に入り追加・削除ボタン--}}
-                            @include('microposts_favorite.favorite_button')
                     </div>
                 </div>
             </li>
